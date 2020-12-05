ff5c3f3d7b48a3d944320ab45652134136268dfb
maspav <maspav18@gmail.com>
2020-12-05 10:01:57 +0000

    corrected google fonts url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
   <title>Form</title>
 
   <!-- CSS -->
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,100,300,500" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,100,300,500" />
   <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
     integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
